0d534c997430ef5d9b679693642be6b8dbab86ea
CaBien-dev <lam-dn@genkisystem>
2024-06-12 14:20:21 +0700

    feat: add config prompt, on/off claude

--- modules/Email/src/main/java/email/configs/EmailConfigKeys.java
+++ modules/Email/src/main/java/email/configs/EmailConfigKeys.java
@@ -6,4 +6,7 @@ public class EmailConfigKeys {
     public static final String GG_SECRET_KEY = "googleSecretKey";
     public static final String CLAUDE_API_KEY = "claudeAPIKey";
     public static final String GG_ACCESS_TOKEN = "ggAccessToken";
+    public static final String IS_USE_CLAUDE_AI = "isUseClaudeAI";
+    public static final String PROMPT_SUMMARY = "promptSummary";
+    public static final String PROMPT_SUGGESTION = "promptSuggestion";
 }
--- modules/Email/src/main/java/email/controller/EmailController.java
+++ modules/Email/src/main/java/email/controller/EmailController.java
@@ -2,21 +2,15 @@ package email.controller;
 
 import com.liferay.portletmvc4spring.bind.annotation.ActionMapping;
 import com.liferay.portletmvc4spring.bind.annotation.RenderMapping;
-import email.dto.ClaudeMailResDTO;
 import email.dto.EmailDTO;
-import email.dto.EmailDTOReq;
 import email.dto.User;
 import email.services.EmailServiceImpl;
-import email.utils.EmailConfigs;
+import email.utils.EmailPortletConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -24,8 +18,6 @@ import javax.portlet.ActionResponse;
 import javax.portlet.MutableRenderParameters;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletSession;
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -47,15 +39,11 @@ public class EmailController {
 	public String prepareView(ModelMap modelMap, PortletRequest portletRequest) throws Exception {
 		_logger.info("prepareView");
 
-		EmailConfigs emailConfigs = new EmailConfigs();
-		emailConfigs.updateProps(portletRequest);
-		_logger.info("Google Client Key: {}", !emailConfigs.getGoogleClientKey().isEmpty() ? "******" : "Empty");
-		_logger.info("Google Secret Key: {}", !emailConfigs.getGoggleSecretKey().isEmpty() ? "******": "Empty");
-		_logger.info("Claude API Key: {}", !emailConfigs.getClaudeAPIKey().isEmpty() ? "******" : "Empty");
-		_logger.info("Google Access Token: {}", !emailConfigs.getGgAccessToken().isEmpty() ? "******" : "Empty");
+		EmailPortletConfigs emailPortletConfigs = new EmailPortletConfigs();
+		emailPortletConfigs.updateProps(portletRequest);
 
 		try {
-            return emailService.renderService(modelMap, portletRequest, emailConfigs, null);
+            return emailService.renderService(modelMap, portletRequest, emailPortletConfigs, null);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "error";
@@ -91,10 +79,10 @@ public class EmailController {
 		emailDTO.setId(emailId);
 		emailDTO.setThreadId(threadId);
 
-		EmailConfigs emailConfigs = new EmailConfigs();
-		emailConfigs.updateProps(portletRequest);
+		EmailPortletConfigs emailPortletConfigs = new EmailPortletConfigs();
+		emailPortletConfigs.updateProps(portletRequest);
 
-		String view = emailService.renderService(modelMap, portletRequest, emailConfigs, emailDTO);
+		String view = emailService.renderService(modelMap, portletRequest, emailPortletConfigs, emailDTO);
 
 		return view;
 	}
