79cbe1b276368623fb47d9f0e1575e0ebee341ca
CaBien-dev <lam-dn@genkisystem>
2024-06-24 15:12:13 +0700

    feat: open summary

--- modules/Email/src/main/java/email/services/EmailService.java
+++ modules/Email/src/main/java/email/services/EmailService.java
@@ -27,7 +27,7 @@ public class EmailService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    public String renderService(ModelMap modelMap, EmailPortletConfigs emailPortletConfigs, EmailDTO currentEmail) {
+    public String renderService(ModelMap modelMap, EmailPortletConfigs emailPortletConfigs, EmailDTO currentEmail) throws ExecutionException, InterruptedException {
         LOGGER.info("[RENDER SERVICE] - running ...");
 
         String accessToken = emailPortletConfigs.getGgAccessToken();
@@ -55,34 +55,34 @@ public class EmailService {
             modelMap.put("labelsGG", listLabel);
             modelMap.put("labelsAI", aiLabelList);
 
-//            String bodyText;
-//            boolean isThread ;
-//            if (currentEmail.getId().equals(currentEmail.getThreadId())) {
-//                bodyText = emailDTOOptional.get().getBodyPlainText();
-//                if (bodyText == null || bodyText.isEmpty()) {
-//                    bodyText = emailDTOOptional.get().getBodyHtml();
-//                }
-//                isThread = false;
-//            } else {
-//                bodyText = gmailService.getGmailThreadDetail(accessToken, currentEmail.getThreadId());
-//                isThread = true;
-//            }
+            String bodyText;
+            boolean isThread ;
+            if (currentEmail.getId().equals(currentEmail.getThreadId())) {
+                bodyText = emailDTOOptional.get().getBodyPlainText();
+                if (bodyText == null || bodyText.isEmpty()) {
+                    bodyText = emailDTOOptional.get().getBodyHtml();
+                }
+                isThread = false;
+            } else {
+                bodyText = gmailService.getGmailThreadDetail(accessToken, currentEmail.getThreadId());
+                isThread = true;
+            }
 
-//            String finalBodyText = bodyText;
-//            CompletableFuture<ClaudeMailResDTO> summaryFuture = CompletableFuture.supplyAsync(() -> aiService.summaryAndSuggestEmail(emailPortletConfigs, finalBodyText, true, isThread));
-//            CompletableFuture<ClaudeMailResDTO> suggestionFuture = CompletableFuture.supplyAsync(() -> aiService.summaryAndSuggestEmail(emailPortletConfigs, finalBodyText, false, isThread));
-//
-//            CompletableFuture.allOf(summaryFuture, suggestionFuture).join();
-//
-//            ClaudeMailResDTO summaryResponse = summaryFuture.get();
-//            ClaudeMailResDTO suggestionResponse = suggestionFuture.get();
-//            if (summaryResponse == null || suggestionResponse == null) {
-//                LOGGER.info("[RENDER SERVICE] - error!");
-//                LOGGER.error("One of the responses is null: summaryResponse={}, suggestionResponse={}", summaryResponse, suggestionResponse);
-//                return "error";
-//            }
-//            modelMap.put("summary", summaryResponse.getContent());
-//            modelMap.put("suggestion", suggestionResponse.getContent());
+            String finalBodyText = bodyText;
+            CompletableFuture<ClaudeMailResDTO> summaryFuture = CompletableFuture.supplyAsync(() -> aiService.summaryAndSuggestEmail(emailPortletConfigs, finalBodyText, true, isThread));
+            CompletableFuture<ClaudeMailResDTO> suggestionFuture = CompletableFuture.supplyAsync(() -> aiService.summaryAndSuggestEmail(emailPortletConfigs, finalBodyText, false, isThread));
+
+            CompletableFuture.allOf(summaryFuture, suggestionFuture).join();
+
+            ClaudeMailResDTO summaryResponse = summaryFuture.get();
+            ClaudeMailResDTO suggestionResponse = suggestionFuture.get();
+            if (summaryResponse == null || suggestionResponse == null) {
+                LOGGER.info("[RENDER SERVICE] - error!");
+                LOGGER.error("One of the responses is null: summaryResponse={}, suggestionResponse={}", summaryResponse, suggestionResponse);
+                return "error";
+            }
+            modelMap.put("summary", summaryResponse.getContent());
+            modelMap.put("suggestion", suggestionResponse.getContent());
         }
         LOGGER.info("[RENDER SERVICE] - done!");
         return "mails";
